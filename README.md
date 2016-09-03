25e75fb05d778d21c8c6d596954178440a3a174d
Arthur M <mur_art@yahoo.com>
2016-09-03 08:17:11 +0200

    added instructions how to run the script in the README, cosmetic changes in the python script.

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # vk2spotify
+
 Scripting to help exporting your Vkontakte playlist into Spotify
 
-You need to:
+This is still in early development (PoC), however it works.
+
+## Installing prerequisites 
+
+Requirements:
+```
+pip install vk_api pyspotify
+```
+
+## Authentication
+
+You need to set the following environment variables:
 
 ```
 export SPOTIPY_CLIENT_ID='your-spotify-client-id'
@@ -10,3 +22,14 @@ export SPOTIPY_REDIRECT_URI='your-app-redirect-url'
 ```
 
 Get your credentials at https://developer.spotify.com/my-applications
+*NOTE*: You may need to create application, if you haven't so far.
+
+Then set the following variables in the **run.py**:
+
+* spotify_user - if you're using Facebook for login, don't put the e-mail, more info here: https://community.spotify.com/t5/Accounts-and-Subscriptions/How-do-i-find-my-username-when-using-Facebook-login/td-p/859795
+* vk_email - this is your vkontakte e-mail address
+* vk_pass - vkontakte password
+* vk_id - you can get the vkontakte id when you login to your account in the
+browser. Then you go to your profile and get the id from the browser URL
+(only the numbers, without 'id')
+
--- run.py
+++ run.py
@@ -10,42 +10,46 @@ import spotipy.util as util
 # Python 2 and 3
 from builtins import input
 
-# provide the user name here
-username = ""
+# provide Spotify user name here
+spotify_user = ''
 
-VK_EMAIL = ''
-VK_ID = ''
-VK_PASS = ''
+# Provide vkontakte email/pass here
+vk_email = ''
+vk_pass = ''
+
+# when you sign-in to your vkontakte account in browser and go to your profile,
+# it shows you the id in the URL. 
+vk_id = ''
 
 def get_sms_code():
     code = input('input code from sms\n')
     print('code received: {}'.format(code))
     return code
 
-vk_session = vk_api.VkApi(VK_EMAIL, VK_PASS, auth_handler=get_sms_code)
+vk_session = vk_api.VkApi(vk_email, vk_pass, auth_handler=get_sms_code)
 try:
     vk_session.authorization()
 except vk_api.AuthorizationError as error_msg:
     print(error_msg)
     quit()
 vk = vk_session.get_api()
-songs = vk.audio.get(owner_id=VK_ID)['items']
+songs = vk.audio.get(owner_id=vk_id)['items']
 
 print('You have {} songs'.format(len(songs)))
 
 scope = 'playlist-modify-private'
-token = util.prompt_for_user_token(username, scope)
+token = util.prompt_for_user_token(spotify_user, scope)
 
 if token:
-    print("Got token for {}".format(username))
+    print("Got token for {}".format(spotify_user))
 else:
-    print("Can't get token for {}".format(username))
+    print("Can't get token for {}".format(spotify_user))
     quit()
 
 sp = spotipy.Spotify(auth=token)
 sp.trace = False
 
-playlists = sp.user_playlists(username)
+playlists = sp.user_playlists(spotify_user)
 
 # TODO search the required playlist instead of taking the first one
 playlist = playlists['items'][0]
@@ -58,7 +62,7 @@ for i, song in enumerate(songs):
         items = result['tracks']['items']
         if len(items) > 0:
             print(u"Found: \"{}\", adding to {}".format(items[0]['name'], playlist['name']))
-            sp.user_playlist_add_tracks(username, playlist['uri'], [items[0]['id']])
+            sp.user_playlist_add_tracks(spotify_user, playlist['uri'], [items[0]['id']])
             break
         else:
             print(u"Not found")
